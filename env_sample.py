696fa385999151462ad7a5398d3b9456ea8305e6
TomasErudito <tomas@oppossite.com>
2022-04-21 10:58:35 +0000

    connect heroku

---
+++ env_sample.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "k&<+b=`d,iS`$`!GvLs@_M,ywycs,g")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "groundDB")
