d7a195af8192dd11a1c2b8850ee8113366aa04e0
TomasErudito <tomas@oppossite.com>
2022-04-21 11:51:27 +0000

    password update

--- env_sample.py
+++ env_sample.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "k&<+b=`d,iS`$`!GvLs@_M,ywycs,g")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://grounduser:123malutka@groundcluster.tjbnw.mongodb.net/groundDB?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://grounduser:Karakolito1@groundcluster.tjbnw.mongodb.net/groundDB?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "groundDB")
